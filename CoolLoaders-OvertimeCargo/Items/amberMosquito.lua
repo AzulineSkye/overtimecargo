b5d7e08741917d8067583db3507ba54ef909a4ac
Mr. Marco <65143426+LosMarquitos@users.noreply.github.com>
2025-01-26 08:10:05 +0000

    removed prints

--- CoolLoaders-OvertimeCargo/Items/amberMosquito.lua
+++ CoolLoaders-OvertimeCargo/Items/amberMosquito.lua
@@ -11,7 +11,6 @@ Callback.add(Callback.TYPE.onEnemyInit, "amberReduction", function(enemy)
 	for _, player in ipairs(Instance.find_all(gm.constants.oP)) do
 		local count = player:item_stack_count(amber)
 		if count >= 1 then
-			print(enemy.maxhp)
 			for i = 1, count, 1 do
 				if i == 1 then
 					enemy.maxhp = enemy.maxhp * 0.75
@@ -21,7 +20,6 @@ Callback.add(Callback.TYPE.onEnemyInit, "amberReduction", function(enemy)
 					enemy.hp = enemy.maxhp
 				end
 			end
-			print(enemy.maxhp)
 		end
 	end
 end)
